38ba377474387f04eaee2e19e8835657937a6735
buyeah1109 <buyeah1109@gmail.com>
2021-03-14 16:34:29 +0800

    Initialization

---
+++ Assets/Scripts/Cell2048.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Cell2048 : MonoBehaviour
+{
+    [SerializeField] Cell2048 up;
+    [SerializeField] Cell2048 down;
+    [SerializeField] Cell2048 left;
+    [SerializeField] Cell2048 right;
+
+    [SerializeField] GameObject fillPrefab;
+
+    private void OnEnable() {
+        GameController2048.slide += OnSlide;
+        GameController2048.checkDeath += CheckDeath;
+    }
+
+    private void OnDisable() {
+        GameController2048.slide -= OnSlide;
+        GameController2048.checkDeath -= CheckDeath;
+    }
+    
+    private void OnSlide(string command)
+    {
+        if(command.Equals("w"))
+            slideUp();
+        else if(command.Equals("s"))
+            slideDown();
+        else if(command.Equals("a"))
+            slideLeft();
+        else if(command.Equals("d"))
+            slideRight();
+    }
+    private void CheckDeath() {
+        if(this.up != null){
+            if(this.transform.childCount == 0 || this.up.transform.childCount == 0)
+                return;
+            if(this.up.transform.childCount != 0) {
+                String thisValue = this.transform.GetChild(0).GetChild(0).GetComponent<Text>().text;
+                String upValue = this.up.transform.GetChild(0).GetChild(0).GetComponent<Text>().text;
+                if(thisValue.Equals(upValue))
+                    return;
+            }   
+        }
+        if(this.down != null){
+            if(this.transform.childCount == 0 || this.down.transform.childCount == 0)
+                return;
+            if(this.down.transform.childCount != 0) {
+                String thisValue = this.transform.GetChild(0).GetChild(0).GetComponent<Text>().text;
+                String upValue = this.down.transform.GetChild(0).GetChild(0).GetComponent<Text>().text;
+                if(thisValue.Equals(upValue))
+                    return;
+            }   
+        }
+        if(this.left != null){
+            if(this.transform.childCount == 0 || this.left.transform.childCount == 0)
+                return;
+            if(this.left.transform.childCount != 0) {
+                String thisValue = this.transform.GetChild(0).GetChild(0).GetComponent<Text>().text;
+                String upValue = this.left.transform.GetChild(0).GetChild(0).GetComponent<Text>().text;
+                if(thisValue.Equals(upValue))
+                    return;
+            }   
+        }
+        if(this.right != null){
+            if(this.transform.childCount == 0 || this.right.transform.childCount == 0)
+                return;
+            if(this.right.transform.childCount != 0) {
+                String thisValue = this.transform.GetChild(0).GetChild(0).GetComponent<Text>().text;
+                String upValue = this.right.transform.GetChild(0).GetChild(0).GetComponent<Text>().text;
+                if(thisValue.Equals(upValue))
+                    return;
+            }   
+        }
+        Debug.Log(this.name + " is dead.");
+        GameController2048.instance.gameOverCheck();
+    }
+    private void slideUp()
+    {
+        Cell2048 currentCell = this;
+        Cell2048 nextCell = currentCell.up;
+        // This cell is filled
+        if(SpawnFilled(currentCell)){
+            Debug.Log(currentCell.name + " moved up");
+
+            // Not in the top
+            if(nextCell != null){
+
+                nextCell.slideUp();
+
+                // Upper Cell is filled also
+                if(SpawnFilled(nextCell) && SpawnFilled(currentCell)){
+                    tryCombine(nextCell, currentCell);
+                }
+                else {
+                    Debug.Log(nextCell.name + " create a new spawn");
+                    GameObject temp = Instantiate(fillPrefab, nextCell.transform);
+                    temp.transform.GetChild(0).gameObject.GetComponent<Text>().text 
+                        = currentCell.transform.GetChild(0).GetChild(0).gameObject.GetComponent<Text>().text;
+                    Debug.Log(currentCell.name + " destroyed");
+                    DestroyImmediate(currentCell.transform.GetChild(0).gameObject);    
+                }
+
+                nextCell.slideUp();
+            }
+        }
+    }
+    private void slideDown()
+    {
+        Cell2048 currentCell = this;
+        Cell2048 nextCell = currentCell.down;
+        // This cell is filled
+        if(SpawnFilled(currentCell)){
+            Debug.Log(currentCell.name + " moved down");
+
+            // Not in the top
+            if(nextCell != null){
+
+                nextCell.slideDown();
+
+                // Upper Cell is filled also
+                if(SpawnFilled(nextCell) && SpawnFilled(currentCell)){
+                    tryCombine(nextCell, currentCell);
+                }
+                else {
+                    Debug.Log(nextCell.name + " create a new spawn");
+                    GameObject temp = Instantiate(fillPrefab, nextCell.transform);
+                    temp.transform.GetChild(0).gameObject.GetComponent<Text>().text 
+                        = currentCell.transform.GetChild(0).GetChild(0).gameObject.GetComponent<Text>().text;
+                    Debug.Log(currentCell.name + " destroyed");
+                    DestroyImmediate(currentCell.transform.GetChild(0).gameObject);    
+                }
+
+                nextCell.slideDown();
+
+            }
+        }
+    }
+    private void slideLeft()
+    {
+        Cell2048 currentCell = this;
+        Cell2048 nextCell = currentCell.left;
+        // This cell is filled
+        if(SpawnFilled(currentCell)){
+            Debug.Log(currentCell.name + " moved up");
+
+            // Not in the top
+            if(nextCell != null){
+
+                nextCell.slideLeft();
+
+                // Upper Cell is filled also
+                if(SpawnFilled(nextCell) && SpawnFilled(currentCell)){
+                    tryCombine(nextCell, currentCell);
+                }
+                else {
+                    Debug.Log(nextCell.name + " create a new spawn");
+                    GameObject temp = Instantiate(fillPrefab, nextCell.transform);
+                    temp.transform.GetChild(0).gameObject.GetComponent<Text>().text 
+                        = currentCell.transform.GetChild(0).GetChild(0).gameObject.GetComponent<Text>().text;
+                    Debug.Log(currentCell.name + " destroyed");
+                    DestroyImmediate(currentCell.transform.GetChild(0).gameObject);    
+                }
+
+                nextCell.slideLeft();
+
+            }
+        }
+    }
+    private void slideRight()
+    {
+        Cell2048 currentCell = this;
+        Cell2048 nextCell = currentCell.right;
+        // This cell is filled
+        if(SpawnFilled(currentCell)){
+            Debug.Log(currentCell.name + " moved up");
+
+            // Not in the top
+            if(nextCell != null){
+
+                nextCell.slideRight();
+
+                // Upper Cell is filled also
+                if(SpawnFilled(nextCell) && SpawnFilled(currentCell)){
+                    tryCombine(nextCell, currentCell);
+                }
+                else {
+                    Debug.Log(nextCell.name + " create a new spawn");
+                    GameObject temp = Instantiate(fillPrefab, nextCell.transform);
+                    temp.transform.GetChild(0).gameObject.GetComponent<Text>().text 
+                        = currentCell.transform.GetChild(0).GetChild(0).gameObject.GetComponent<Text>().text;
+                    Debug.Log(currentCell.name + " destroyed");
+                    DestroyImmediate(currentCell.transform.GetChild(0).gameObject);    
+                }
+
+                nextCell.slideRight();
+
+            }
+        }
+    }
+    private bool SpawnFilled(Cell2048 cell){
+        return cell.transform.childCount != 0;
+    }
+    private void tryCombine(Cell2048 combineIn, Cell2048 combineOut){
+        Text Celltext_in = combineIn.transform.GetChild(0).GetChild(0).gameObject.GetComponent<Text>();
+        Text Celltext_out = combineOut.transform.GetChild(0).GetChild(0).gameObject.GetComponent<Text>();
+        if(Celltext_in.text.Equals(Celltext_out.text)){
+            Debug.Log("Combined" + combineOut.name + " to " +combineIn.name);
+            Celltext_in.text = (int.Parse(Celltext_out.text) + int.Parse(Celltext_in.text)).ToString();
+            DestroyImmediate(combineOut.transform.GetChild(0).gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/GameController2048.cs
@@ -0,0 +1,131 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using UnityEngine.SceneManagement;
+
+public class GameController2048 : MonoBehaviour
+{
+    [SerializeField] GameObject fillPrefab;
+    [SerializeField] Transform[] allCells;
+    // Start is called before the first frame update
+    [SerializeField] GameObject GameOverPanel;
+    [SerializeField] GameObject restartButton;
+
+    public static Action<String> slide;
+    public static Action checkDeath;
+    public static GameController2048 instance;
+    public Color[] colors;
+
+    private int GameOverCount = 0;
+
+    void Start()
+    {
+        instance = this;
+        GameOverPanel.SetActive(false);
+        Button btn = restartButton.GetComponent<Button>();
+        btn.onClick.AddListener(restart);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space)){
+            GameOverCount = 0;
+            fill();
+            checkDeath();
+
+        }
+        if(Input.GetKeyDown(KeyCode.A)){
+            GameOverCount = 0;
+            slide("a");
+            checkDeath();
+        }
+        if(Input.GetKeyDown(KeyCode.S)){
+            GameOverCount = 0;
+            slide("s");
+            checkDeath();
+        } 
+        if(Input.GetKeyDown(KeyCode.W)){
+            GameOverCount = 0;
+            slide("w");
+            checkDeath();
+        } 
+        if(Input.GetKeyDown(KeyCode.D)){
+            GameOverCount = 0;
+            slide("d");
+            checkDeath();
+        }
+        fillColor();
+    }
+    
+    public void fill(){
+
+        float chance = UnityEngine.Random.Range(0f,1f);
+        int whichSpawn = UnityEngine.Random.Range(0, allCells.Length);
+        bool allfilled = true;
+
+        for(int i=0;i<allCells.Length;i++){
+            if(allCells[i].childCount == 0) {
+                allfilled = false;
+                break;
+            }
+        }
+        if(allfilled)
+            return;
+
+        // Already filled
+        if(allCells[whichSpawn].childCount != 0){
+            Debug.Log(allCells[whichSpawn].name + " Already Filled");
+            fill();
+            return;
+        }
+
+        if(chance < .7f){
+            GameObject temp = Instantiate(fillPrefab, allCells[whichSpawn]);
+            Text fillText = temp.transform.GetChild(0).gameObject.GetComponent<Text>();
+            fillText.text = "2";
+            Debug.Log(2);
+        }
+        else {
+            GameObject temp = Instantiate(fillPrefab, allCells[whichSpawn]);
+            Text fillText = temp.transform.GetChild(0).gameObject.GetComponent<Text>();
+            fillText.text = "4";
+            Debug.Log(4);
+        }
+    }
+    private void fillColor() {
+        foreach (Transform i in allCells){
+            if(i.childCount != 0){
+                Text myValue = i.GetChild(0).GetChild(0).gameObject.GetComponent<Text>();
+                int colorIndex = getColorIndex(myValue.text);
+                Color fillColor = colors[colorIndex];
+                i.GetChild(0).GetComponent<Image>().color = fillColor;
+            }     
+        }     
+    }
+    private int getColorIndex(String myValue){
+        int index = 0;
+        int value = int.Parse(myValue);
+        while(value / 2 != 1){
+            index += 1;
+            value /= 2;
+        }
+        return index;
+    }
+
+    private void gameOver(){
+        GameOverPanel.SetActive(true);
+    }
+
+    private void restart(){
+        SceneManager.LoadScene(0);
+    }
+    
+    public void gameOverCheck(){
+        GameOverCount++;
+        if(GameOverCount == allCells.Length) 
+            gameOver();
+    }
+}
